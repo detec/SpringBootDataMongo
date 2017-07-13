1348f6a29bdaf773f7c8eefb264724dc0215245e
WINMAIN\Andrii <andrii.duplyk@nitka.com>
2017-07-13 10:50:38 +0300

    Simplified package scanning

--- src/main/java/sample/Application.java
+++ src/main/java/sample/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Andrii Duplyk
  *
  */
-@SpringBootApplication(scanBasePackages = { "sample.util", "sample.dao", "sample.controller", "sample.config" })
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
